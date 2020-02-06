9fae0d0cb7db13bc7847b81275d577b7e34ad5b3
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-06 19:01:47 +0900

    Lsコマンド

--- src/Ls.java
+++ src/Ls.java
@@ -3,15 +3,11 @@ import java.io.File;
 public class Ls {
     public static void main(String[] args) {
         String path = System.getProperty("user.dir");
-        System.out.println(path);
-        File[] files = path.listFiles();
+        File dir = new File(path);
+        File[] files = dir.listFiles();
 
-        String fileName = new File(path).getName();
-        System.out.println();
         for (int i = 0; i < files.length; i++) {
-            File file = files[i];
-            String fileName = new File(path).getName();
-            System.out.println(fileName);
+            System.out.println(files[i].getName());
         }
     }
 }
