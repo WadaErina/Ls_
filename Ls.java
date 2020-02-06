56e5b53545a558bdfcaeda07def4d1819e4aa489
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-06 15:29:04 +0900

    no message

--- Ls.java
+++ Ls.java
@@ -1,9 +1,9 @@
-public class Command {
-    public static void main(String[] args) {
-        try {
-            Runtime runtime = Runtime.getRuntime();
-            runtime.exec("ls");
-        } catch (IOException ex) {
-        }
-    }
+public class Ls {
+  public static void main(String[] args) {
+  try {
+      Runtime runtime = Runtime.getRuntime();
+      runtime.exec("ls");
+  } catch (IOException ex) {
+  }
+}
 }
