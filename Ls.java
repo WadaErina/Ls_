cd8607bc31345cff76cdb41f4145e217d57cfda0
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-06 15:40:14 +0900

    test

--- Ls.java
+++
@@ -1,9 +0,0 @@
-public class Ls {
-  public static void main(String[] args) {
-  try {
-      Runtime runtime = Runtime.getRuntime();
-      runtime.exec("ls");
-  } catch (IOException ex) {
-  }
-}
-}
