0232aed9fa81247248a1cfaeaee1175966ce1179
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-06 15:15:52 +0900

    no message

--- Ls.java
+++ Ls.java
@@ -1,10 +1,9 @@
-public static void main(String[] args) {
-    try {
-        Runtime runtime = Runtime.getRuntime();
-        Process p = runtime.exec("ls");
-        InputStream is = p.getInputStream();
-        // InputStreamさえ得られれば、後は必要に応じて内容を取得すればよい。
-        // 以下、省略
-    } catch (IOException ex) {
+public class Command {
+    public static void main(String[] args) {
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            runtime.exec("ls");
+        } catch (IOException ex) {
+        }
     }
 }
