0a08112749101ad3dc7e1f06edcaa89052059029
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-07 18:25:46 +0900

    絶対パスのディレクトリのファイル一覧を表示する

--- src/Ls.java
+++ src/Ls.java
@@ -1,13 +1,34 @@
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileOwnerAttributeView;
+import java.text.SimpleDateFormat;
+import java.nio.file.attribute.UserPrincipal;
 
 public class Ls {
     public static void main(String[] args) {
+        // currentディレクトリのファイル一覧を表示する
         String path = System.getProperty("user.dir");
+        System.out.println(path);
         File dir = new File(path);
         File[] files = dir.listFiles();
 
         for (int i = 0; i < files.length; i++) {
             System.out.println(files[i].getName());
+        }
+        //   絶対パスのディレクトリのファイル一覧を表示する
+        System.out.println(args[0]);
+        File dir2 = new File(args[0]);
+        File[] files2 = dir2.listFiles();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd HH:mm");
+
+        for (int i = 0; i < files2.length; i++) {
+            UserPrincipal owner = Files.getOwner(path);
+            System.out.println(simpleDateFormat.format(files2[i].lastModified()));
+            System.out.println(files2[i].getName());
+            System.out.println(files2[i].length());
+            System.out.println(files2[i].getOwner());
         }
     }
 }
