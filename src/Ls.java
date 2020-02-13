347a7dd87364529155b4f390dd98f61c01d826cd
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-13 18:53:41 +0900

    オプション-lに対応

--- src/Ls.java
+++ src/Ls.java
@@ -1,34 +1,112 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileOwnerAttributeView;
 import java.text.SimpleDateFormat;
-import java.nio.file.attribute.UserPrincipal;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Set;
 
 public class Ls {
-    public static void main(String[] args) {
-        // currentディレクトリのファイル一覧を表示する
-        String path = System.getProperty("user.dir");
-        System.out.println(path);
-        File dir = new File(path);
-        File[] files = dir.listFiles();
+    public static void main(String[] args) throws IOException {
+        System.out.println(args[1]);
+        if (args[0].contains("~/") || args[1].contains("~/")) {                  // 絶対パスが送られてきたとき
+            File dir = new File(args[0]);
+            File[] files = dir.listFiles();
 
-        for (int i = 0; i < files.length; i++) {
-            System.out.println(files[i].getName());
-        }
-        //   絶対パスのディレクトリのファイル一覧を表示する
-        System.out.println(args[0]);
-        File dir2 = new File(args[0]);
-        File[] files2 = dir2.listFiles();
+            if (args.length == 0) {                                                                 // lsコマンドのみのとき
+                for (int i = 0; i < files.length; i++) {
+                    System.out.print(files[i].getName() + " ");
+                }
+                System.out.println();
+            } else if (args[0].contains("l") && args[0].contains("-")) {                            // オプションがあるとき
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd HH:mm");
+                for (int i = 0; i < files.length; i++) {
+                    // ファイルの種類
+                    if (files[i].isFile()) {
+                        System.out.print("-");
+                    } else if (files[i].isDirectory()) {
+                        System.out.print("d");
+                    } else {
+                        System.out.print("l");
+                    }
+                    Set<PosixFilePermission> permissions = Files.getPosixFilePermissions(files[i].toPath());
+                    System.out.print(PosixFilePermissions.toString(permissions) + " ");
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd HH:mm");
+                    // ハードリンク数
+                    if (files[i].isDirectory()) {
+                        System.out.print(String.format("%3s", files[i].list().length + 2 + " "));
+                    } else {
+                        System.out.print(String.format("%3s", "1" + " "));
+                    }
 
-        for (int i = 0; i < files2.length; i++) {
-            UserPrincipal owner = Files.getOwner(path);
-            System.out.println(simpleDateFormat.format(files2[i].lastModified()));
-            System.out.println(files2[i].getName());
-            System.out.println(files2[i].length());
-            System.out.println(files2[i].getOwner());
+                    // 所有者と所有グループ
+                    System.out.print(Files.getOwner(files[i].toPath()) + " ");
+                    System.out.print(Files.getFileAttributeView(files[i].toPath(), PosixFileAttributeView.class).readAttributes().group().getName() + " ");
+
+                    // ファイルサイズ
+                    System.out.print(String.format("%5s", files[i].length()) + " ");
+
+                    // タイムスタンプ
+                    System.out.print(simpleDateFormat.format(files[i].lastModified()) + " ");
+
+                    // ファイル名
+                    System.out.print(files[i].getName() + " ");
+
+                    System.out.println();
+                }
+            } else if (args[0].contains("a") && args[0].contains("-")) {
+
+            }
+        } else {      // カレントディレクトリから
+            String path = System.getProperty("user.dir");
+            File dir = new File(path);
+            File[] files = dir.listFiles();
+
+            if (args.length == 0) {                                                                 // lsコマンドのみのとき
+                for (int i = 0; i < files.length; i++) {
+                    System.out.print(files[i].getName() + " ");
+                }
+                System.out.println();
+            } else if (args[0].contains("l") && args[0].contains("-")) {                            // オプションがあるとき
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd HH:mm");
+                for (int i = 0; i < files.length; i++) {
+                    // ファイルの種類
+                    if (files[i].isFile()) {
+                        System.out.print("-");
+                    } else if (files[i].isDirectory()) {
+                        System.out.print("d");
+                    } else {
+                        System.out.print("l");
+                    }
+                    Set<PosixFilePermission> permissions = Files.getPosixFilePermissions(files[i].toPath());
+                    System.out.print(PosixFilePermissions.toString(permissions) + " ");
+
+                    // ハードリンク数
+                    if (files[i].isDirectory()) {
+                        System.out.print(String.format("%3s", files[i].list().length + 2 + " "));
+                    } else {
+                        System.out.print(String.format("%3s", "1" + " "));
+                    }
+
+                    // 所有者と所有グループ
+                    System.out.print(Files.getOwner(files[i].toPath()) + " ");
+                    System.out.print(Files.getFileAttributeView(files[i].toPath(), PosixFileAttributeView.class).readAttributes().group().getName() + " ");
+
+                    // ファイルサイズ
+                    System.out.print(String.format("%5s", files[i].length()) + " ");
+
+                    // タイムスタンプ
+                    System.out.print(simpleDateFormat.format(files[i].lastModified()) + " ");
+
+                    // ファイル名
+                    System.out.print(files[i].getName() + " ");
+
+                    System.out.println();
+                }
+            } else if (args[0].contains("a") && args[0].contains("-")) {
+
+            }
         }
     }
 }
